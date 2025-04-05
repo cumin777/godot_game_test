51273409ee169f75108ad4fda8c4ee3ca0e2c883
Jeremi Biernacki <jeremi360@gmail.com>
2025-04-05 11:21:32 +0200

    add new easy to customize ChoiceButton for Menu

--- scenes/DialogueUI/MenuBox.gd
+++ scenes/DialogueUI/MenuBox.gd
@@ -1,6 +1,7 @@
 extends VBoxContainer
 
-@export var parent: Control
+@export var root: Control
+@export var choice_btn_scene: PackedScene
 
 var markup : TextParser
 
@@ -8,30 +9,21 @@ signal menu_ready
 signal menu_clean
 
 func _ready():
-	if Engine.is_editor_hint():
-		return
-	
 	markup = load(VisualNovelKit.default_markup_setting)
 	Rakugo.sg_menu.connect(_on_menu)
-	
-	if parent:
-		visibility_changed.connect(_on_visibility_changed)
-	
+	if root: visibility_changed.connect(_on_visibility_changed)
 	hide()
 
 func _on_visibility_changed():
-	parent.visible = visible
+	root.visible = visible
 
 func _on_menu(choices:Array):
-	# for id in choices.size():
-	# 	var choice_btn := AdvancedTextButton.new()
-	# 	add_child(choice_btn)
-	# 	choice_btn.fit_content = true
-	# 	choice_btn.scroll_active = false
-	# 	choice_btn.shortcut_keys_enabled = false
-	# 	choice_btn.parser = markup
-	# 	choice_btn._text = choices[id]
-	# 	choice_btn.pressed.connect(_on_choice.bind(id))
+	for id in choices.size():
+		var choice_btn := choice_btn_scene.instantiate()
+		add_child(choice_btn)
+		choice_btn.parser = markup
+		choice_btn.text = choices[id]
+		choice_btn.pressed.connect(_on_choice.bind(id))
 	
 	menu_ready.emit()
 	show()
@@ -39,6 +31,5 @@ func _on_menu(choices:Array):
 func _on_choice(id: int):
 	Rakugo.menu_return(id)
 	hide()
-	for ch in get_children():
-		ch.queue_free()
+	for ch in get_children(): ch.queue_free()
 	menu_clean.emit()
---
+++ scenes/DialogueUI/choice_button.gd
@@ -0,0 +1,23 @@
+@tool
+class_name ChoiceButton
+extends ButtonContainer
+
+@export var label: AdvancedTextLabel:
+	set(value):
+		label = value
+		if !is_node_ready(): await ready
+		if label:
+			label.parser = parser
+			label.text = text
+
+@export var parser: TextParser:
+	set(value):
+		parser = value
+		if !is_node_ready(): await ready
+		if label: label.parser = value
+
+@export_multiline var text := "":
+	set(value):
+		text = value
+		if !is_node_ready(): await ready
+		if label: label.advanced_text = value
